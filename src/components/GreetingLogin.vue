68bc6d4ac9f598c23e7f3eaf82d41852145b249c
iop2589 <qkrtjdcjf07@naver.com>
2022-10-24 13:39:22 +0900

    momentum 개발 시작 greeting 로직 중간 커밋

---
+++ src/components/GreetingLogin.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div v-if="!isLogin">
+            <form v-on:submit.prevent="onLoginSubmit" id="login-form" class="hidden todos-form">
+                <input v-model="userName" required maxlength="15" type="text" placeholder="What is your name?" />
+                <button class="font-color todos-input">Log In</button>
+            </form>
+        </div>
+        <div class="greeting" v-else>
+            <h1 id="greeting">Hello ! {{ userName }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+
+const USERNAME_KEY = "userName";
+export default {
+    data: function () {
+        return {
+            userName: "",
+            isLogin: false
+        }
+    },
+    methods: {
+        onLoginSubmit: function () {
+            localStorage.setItem(USERNAME_KEY, this.userName);
+            this.isLogin = true;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
